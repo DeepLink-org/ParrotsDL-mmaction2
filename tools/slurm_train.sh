ffca5914bb09439bcbd16f75c31fee22a506475b
youhuijie <st11279@qq.com>
2022-12-19 16:59:47 +0800

    for eval

--- tools/slurm_train.sh
+++ tools/slurm_train.sh
@@ -15,10 +15,10 @@ PY_ARGS=${@:4}  # Any arguments from the forth one are captured by this
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 srun -p ${PARTITION} \
     --job-name=${JOB_NAME} \
-    --gres=gpu:${GPUS_PER_NODE} \
+    --gres=mlu:${GPUS_PER_NODE} \
     --ntasks=${GPUS} \
     --ntasks-per-node=${GPUS_PER_NODE} \
     --cpus-per-task=${CPUS_PER_TASK} \
     --kill-on-bad-exit=1 \
     ${SRUN_ARGS} \
-    python -u tools/train.py ${CONFIG} --launcher="slurm" ${PY_ARGS}
+    python -u tools/train.py ${CONFIG} --launcher="slurm" --validate ${PY_ARGS}
